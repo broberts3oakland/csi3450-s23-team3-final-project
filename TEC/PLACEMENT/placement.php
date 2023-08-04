4abca0f0a5c85e58fb73a3393b3fe926c05e860b
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2023-08-04 08:03:04 -0400

    Update placement.php
    Deleted comments

--- TEC/PLACEMENT/placement.php
+++ TEC/PLACEMENT/placement.php
@@ -35,31 +35,12 @@ if ($conn->query($sql) === TRUE)
 }
 
 SELECT_All_from_Query($conn, $Table_Name);
-// $sql = "SELECT * FROM {$Table_Name}"; 
 
-// $result = $conn->query($sql);
-// if ($result->num_rows > 0) 
-//   {
-
-//     //Returns an array representing the fetched row, where each key in the array represents the name of one of the result set's columns
-//     $row = $result->fetch_assoc();
-
-//     echo "<br> Opening Number {$row['Opening_Number']} was logged";
-//     echo "<br> Candidate_Number {$row['Candidate_Number']} was logged";
-//     echo "<br> Total_Hours_Worked {$row['Total_Hours_Worked']} was logged";
-
-//   } 
-//   else 
-//   {
-//     echo "<br> 0 results";
-//   }
-
-
-  echo "<form class='row g-2' action='placement_table.php' method='GET'>";
-  echo "<div class='col-auto'>";
-  echo "<button type='submit' class='btn btn-primary mb-3'>Display Table</button>";
-  echo "</div>";
-  echo "</form>";
+echo "<form class='row g-2' action='placement_table.php' method='GET'>";
+echo "<div class='col-auto'>";
+echo "<button type='submit' class='btn btn-primary mb-3'>Display Table</button>";
+echo "</div>";
+echo "</form>";
 
     // ?>
   </body>
