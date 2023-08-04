f5bb67c45e5b52202386d21b275050b16f289ba9
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2023-08-04 10:17:56 -0400

    Fixed html
    Added bootstrap import to each file with html

--- TEC/PLACEMENT/placement_table.php
+++ TEC/PLACEMENT/placement_table.php
@@ -4,6 +4,8 @@
   <head>
     <meta charset="utf-8">
     <title>A PHP Script for Testing the GET Protocol</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </head>
   <body>
     <?php
@@ -17,13 +19,12 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) 
   {
 
-    echo "<table class='table'>";
+    echo "<table class='table table-bordered'>";
     echo "<thead>";
     echo   "<tr>";
-    echo     "<th scope='col'>#</th>";
-    echo     "<th scope='col'>First</th>";
-    echo     "<th scope='col'>Last</th>";
-    echo     "<th scope='col'>Handle</th>";
+    echo     "<th scope='col'>Opening Number</th>";
+    echo     "<th scope='col'>Candidate Number</th>";
+    echo     "<th scope='col'>Total Hours Worked</th>";
     echo  "</tr>";
     echo "</thead>";
 
