76c7fed0208d13e267f596cb7e291d18953b389f
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2023-08-10 13:48:30 -0400

    Update create_tables.php
    Created CANDIDATE, COMPANY, and COURSE table

---
+++ TEC/create_tables.php
@@ -0,0 +1,42 @@
+<?php
+function Create_All_Tables($conn)
+{
+
+    $sql = "CREATE TABLE IF NOT EXISTS CANDIDATE(
+        CANDIDATE_ID INT(32),
+        FIRST_NAME VARHCHAR(30),
+        LAST_NAME VARHCHAR(30),
+        AGE INT(32),
+        DATE_OF_REGRISTRATION DATE,
+        JOB_HISTORY INT,
+        COURSE_TAKING INT,
+        QUALIFICATIOIN_CODE INT
+      ) Engine=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+      
+      CREATE TABLE IF NOT EXISTS COMPANY(
+        COMPANY_ID INT,
+        COMPANY_TEXT VARHCHAR(50),
+        EMPLOYEES_REQUESTING INT,
+        OPENING_ID INT
+        ) Engine=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+        CREATE TABLE IF NOT EXISTS COURSE(
+        COURSE_ID INT,
+        SESSION_ID INT,
+        PREREQUISITES VARCHAR(50),
+        QUALIFICATIOIN_CODE VARCHAR(50)
+        ) Engine=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+      
+      
+        ";
+
+    if ($conn->multi_query($sql) === TRUE) 
+    {
+    echo "All tables created successfully";
+    } else 
+    {
+    echo "<br> Couldn't create all tables: " . $conn->error;
+    }
+
+
+}
