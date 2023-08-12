50644d032e4e4033d515b52470b11bf0be260870
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2023-08-12 07:43:36 -0400

    COMPANY table works

--- TEC/COMPANY/company_table.php
+++ TEC/COMPANY/company_table.php
@@ -12,11 +12,9 @@
 require_once('../database.php');
 $conn = Connect_to_Database();
 
-$Table_Name = "PLACEMENT";
+$Table_Name = "COMPANY";
 $sql = "SELECT * FROM {$Table_Name}"; 
 
-
-// TODO Change table structure to match this table
 $result = $conn->query($sql);
 if ($result->num_rows > 0) 
   {
@@ -24,22 +22,19 @@ if ($result->num_rows > 0)
     echo "<table class='table table-bordered table-striped table-dark'>";
     echo "<thead>";
     echo   "<tr>";
-    echo     "<th scope='col'>Opening Number</th>";
-    echo     "<th scope='col'>Candidate Number</th>";
-    echo     "<th scope='col'>Total Hours Worked</th>";
+    echo     "<th scope='col'>COMPANY_ID</th>";
+    echo     "<th scope='col'>COMPANY_NAME</th>";
     echo  "</tr>";
     echo "</thead>";
 
     echo "<tbody>";
-
+    
     // output data of each row
     while($row = $result->fetch_assoc()) 
     {
-  
     echo   "<tr>";
-    echo     "<th scope='row'>{$row['Opening_Number']}</th>";
-    echo     "<td>{$row['Candidate_Number']}</td>";
-    echo     "<td>{$row['Total_Hours_Worked']}</td>";
+    echo     "<th scope='row'>{$row['COMPANY_ID']}</th>";
+    echo     "<td>{$row['COMPANY_NAME']}</td>";
     echo   "</tr>";
 
     }
